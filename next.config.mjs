e7432ab2b31c1fa0c7217b3bab0a1f51de4cdd98
Lex793 <sheldu4enkovaleksey@yandex.ru>
2026-02-23 21:00:47 +0300

    fix: static export for netlify

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
     unoptimized: true,
   },
-  reactStrictMode: true,
+  trailingSlash: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 }
 
 export default nextConfig
