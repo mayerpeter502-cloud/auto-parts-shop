a7662bd9140def4509a551eb7ee7036f3bb8f77e
Lex793 <sheldu4enkovaleksey@yandex.ru>
2026-03-30 22:01:44 +0300

    Fix metadata structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,8 +27,26 @@ export const metadata: Metadata = {
     card: "summary_large_image",
   },
   icons: {
-    icon: "/favicon.ico",
+    icon: [
+      { url: '/favicon.ico' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+    ],
+    apple: [
+      { url: '/apple-touch-icon.png' },
+    ],
+    other: [
+      {
+        rel: 'android-chrome-192x192',
+        url: '/android-chrome-192x192.png',
+      },
+      {
+        rel: 'android-chrome-512x512',
+        url: '/android-chrome-512x512.png',
+      },
+    ],
   },
+  manifest: '/site.webmanifest',
 };
 
 const inter = Inter({ subsets: ['latin', 'cyrillic'] });
